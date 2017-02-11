bb2c44bb4e9193c8f88679e6a3affbcbf2101277
Mahmoud AbuHadr <mahmoud.abuhadr@gmail.com>
2017-02-11 14:43:46 +0200

    added AccountCredentials

---
+++ CIS-website/WebApplication1/account/AccountCredentials.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.account
+{
+    public class AccountCredentials
+    {
+
+        string fName;
+        string lName;
+        string password;
+        string phoneNumber;
+        string email;
+        int id;
+        public void setFName(string firstName) { fName = firstName; }
+        public string getFName() { return fName; }
+        public void setLName(string lastName) { lName = lastName; }
+        public string getLName() { return lName; }
+        public void setPassword(string Password) { password = Password; }
+        public string getPassword() { return password; }
+        public void setPhoneNumber(string PhoneNumber) { phoneNumber = PhoneNumber; }
+        public string getPhoneNumber() { return phoneNumber; }
+        public void setEmail(string Email) { email = Email; }
+        public string getEmail() { return email; }
+        public void setassword(string Password) { password = Password; }
+        public string getpassword() { return password; }
+        public void setId(int Id) { id = Id; }
+        public int getId() { return id; }
+
+
+    }    
+}
