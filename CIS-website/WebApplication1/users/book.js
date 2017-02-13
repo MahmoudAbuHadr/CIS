65b25eac492adc2c5395c83c0eafcffa608184eb
Treika <engmuhammadmoustafa@gmail>
2017-02-13 22:26:56 +0200

    bookpage

---
+++ CIS-website/WebApplication1/users/book.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    var booktime = document.getElementById("TextboxBookTime");
+    booktime.defaultValue = ("18:00");
+    booktime.min = ("18:00");
+    booktime.max = ("20:00");
+    booktime.step = ("1800");
+           
+});
