21de79a5075d53200013b008b8eee04788336057
omarmahmoud96 <omarmahmoud747@gmail.com>
2017-02-14 19:12:53 +0200

    added personalData dao

---
+++ CIS-website/WebApplication1/scripts/personalData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.scripts
+{
+    public class personalData
+    {
+        string firstName;
+        string lastName;
+        string mobile;
+        string gender;
+        string birthday;
+        float height;
+        float weight;
+
+        void setFname(string fname) { firstName = fname;      }
+        string getFname() { return firstName; }
+
+        void setLname(string lname) { lastName = lname; }
+        string getLname() { return lastName; }
+
+        void setMobile(string mobileNum) { mobile = mobileNum; }
+        string getMobile() { return mobile; }
+
+        void setGender(string gen) { gender = gen; }
+        string getGender() { return gender; }
+
+        void setBirthday(string birth) { birthday = birth; }
+        string get() { return firstName; }
+    }
+}
