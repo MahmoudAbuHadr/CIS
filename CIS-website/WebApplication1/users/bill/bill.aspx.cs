7d3a885e7d5e355ecb6633156805004e9b6ed7e6
Mahmoud AbuHadr <mahmoud.abuhadr@gmail.com>
2017-02-16 19:16:02 +0200

    bill page handeled

--- CIS-website/WebApplication1/users/bill/bill.aspx.cs
+++ CIS-website/WebApplication1/users/bill/bill.aspx.cs
@@ -11,6 +11,38 @@ namespace WebApplication1.users.bill
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if ((int)Session["id"] == 0) {
+                Response.Redirect(Page.ResolveClientUrl("../../index.aspx"));
+
+            }
+            else
+            {
+                WebApplication1.scripts.billDAO dao = new scripts.billDAO();
+                int id = (int)Session["id"];
+                List<WebApplication1.scripts.bill> bills = new List<scripts.bill>();
+                bills = dao.getBillByID(id);
+                for (int i = 0; i < bills.Count; i++)
+                {
+                    TableRow row = new TableRow();
+                    TableCell billNumerCell = new TableCell();
+                    TableCell dateCell = new TableCell();
+                    TableCell valueCell = new TableCell();
+                    TableCell paidCell = new TableCell();
+                    billNumerCell.Text = bills[i].getBillNumber().ToString();
+                    dateCell.Text = bills[i].getBDate();
+                    valueCell.Text = bills[i].getValue().ToString();
+                    if (bills[i].getPaid()) paidCell.Text = "Paid";
+                    else paidCell.Text = "Not Paid";
+
+                    row.Cells.Add(billNumerCell);
+                    row.Cells.Add(dateCell);
+                    row.Cells.Add(valueCell);
+                    row.Cells.Add(paidCell);
+                    tableBill.Rows.Add(row);
+                }
+
+            }
+
 
         }
     }
