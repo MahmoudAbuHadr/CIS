f3ff4fbec795e24d3b55274fbc431cd7d34d0035
omarmahmoud96 <omarmahmoud747@gmail.com>
2017-02-17 15:05:50 +0200

    ID changes to id in account table

--- CIS-website/WebApplication1/scripts/AccountDAO.cs
+++ CIS-website/WebApplication1/scripts/AccountDAO.cs
@@ -16,14 +16,14 @@ namespace WebApplication1.scripts
             myConnection.Open();
             SqlCommand myCommand = new SqlCommand();
 
-            myCommand.CommandText = "Select * from Accounts where ID = '" + id + "';";
+            myCommand.CommandText = "Select * from Accounts where id = '" + id + "';";
             myCommand.Connection = myConnection;
             SqlDataReader reader = myCommand.ExecuteReader();
             if (reader.HasRows)
             {
                 while (reader.Read())
                 {
-                    acc.setID(reader.GetInt32(reader.GetOrdinal("ID")));
+                    acc.setID(reader.GetInt32(reader.GetOrdinal("id")));
                     acc.setPhoneNumber(Convert.ToString(reader[1]));
                     acc.setEmail(Convert.ToString(reader[2]));
                     acc.setPassword(Convert.ToString(reader[3]));
@@ -70,7 +70,7 @@ namespace WebApplication1.scripts
 
             myConnection.Open();
             SqlCommand myCommand = new SqlCommand();
-            myCommand.CommandText = "insert into Accounts (id,Pnumber,Email,Password,Fname,Lname) Values ('100','" + acc.getPhoneNumber() + "','" + acc.getEmail() + "','" + acc.getPassword() + "','" + acc.getFName() + "','" + acc.getLName() + "')  ;  ";
+            myCommand.CommandText = "insert into Accounts (Pnumber,Email,Password,Fname,Lname) Values ('" + acc.getPhoneNumber() + "','" + acc.getEmail() + "','" + acc.getPassword() + "','" + acc.getFName() + "','" + acc.getLName() + "')  ;  ";
             myCommand.Connection = myConnection;
             myCommand.ExecuteNonQuery();
             myConnection.Close();
@@ -83,7 +83,7 @@ namespace WebApplication1.scripts
 
             myConnection.Open();
             SqlCommand myCommand = new SqlCommand();
-            myCommand.CommandText = "UPDATE Accounts SET ID =" + acc.getID().ToString() + ",PNumber ='" + acc.getPhoneNumber() + "',Email ='" + acc.getEmail() + "', Password ='" + acc.getPassword() + "', FName = '" + acc.getFName() + "', LName = '" + acc.getLName() + "' WHERE ID = " + acc.getID() + ";";
+            myCommand.CommandText = "UPDATE Accounts SET id =" + acc.getID().ToString() + ",PNumber ='" + acc.getPhoneNumber() + "',Email ='" + acc.getEmail() + "', Password ='" + acc.getPassword() + "', FName = '" + acc.getFName() + "', LName = '" + acc.getLName() + "' WHERE id = " + acc.getID() + ";";
             myCommand.Connection = myConnection;
             myCommand.ExecuteNonQuery();
             myConnection.Close();
